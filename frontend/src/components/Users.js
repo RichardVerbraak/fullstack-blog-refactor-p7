b40fc357ff9dbf09a9dd40501956b24afd10c574
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-07-01 17:55:32 +0200

    Users component made

---
+++ frontend/src/components/Users.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const Users = () => {
+	const dispatch = useDispatch()
+
+	return (
+		<div>
+			<h2>Users</h2>
+		</div>
+	)
+}
+
+export default Users
