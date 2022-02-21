965a421fb09683c6f2d80f97da329a5cfeeca2b8
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-02-21 17:29:49 +0100

    Added a readme to provide the login credentials

---
+++ README.md
@@ -0,0 +1,24 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Scripts
+
+In the project directory, you can run:
+
+### `npm run dev`
+
+This will run the App in development mode
+
+### Login credentials
+
+To login into the app you can use the following credentials
+
+Username: Hank
+Password: hank123
+
+Username: Bobby
+Password: bobby123
+
+Username: Peggy
+Password: peggy123
--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,42 @@
-const express = require('express')
-const http = require('http')
-const connectDB = require('./config/db')
-const cors = require('cors')
-const dotenv = require('dotenv')
+const express = require('express');
+const http = require('http');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
-const blogRoutes = require('./routes/blogRoutes')
-const userRoutes = require('./routes/userRoutes')
-const testRoutes = require('./routes/testRoutes')
+const blogRoutes = require('./routes/blogRoutes');
+const userRoutes = require('./routes/userRoutes');
+const testRoutes = require('./routes/testRoutes');
 
-const errorHandler = require('./middleware/errorMiddleware')
-const tokenExtractor = require('./middleware/tokenMiddleware')
+const errorHandler = require('./middleware/errorMiddleware');
+const tokenExtractor = require('./middleware/tokenMiddleware');
 
-const app = express()
+const app = express();
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(tokenExtractor)
+app.use(tokenExtractor);
 
 if (process.env.NODE_ENV === 'test') {
-	app.use('/api/test', testRoutes)
+	app.use('/api/test', testRoutes);
 }
 
-app.use('/api/blogs', blogRoutes)
-app.use('/api/users', userRoutes)
+app.use('/api/blogs', blogRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3003;
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-})
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
 
-module.exports = server
+module.exports = server;
